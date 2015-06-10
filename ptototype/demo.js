1faa53c35e9d762f1d508d53f7f7a532b3c67c0a
KongDongbin <kkkdb@users.noreply.github.com>
2015-06-10 23:29:44 +0800

    Create demo.js

---
+++ ptototype/demo.js
@@ -0,0 +1,46 @@
+//接下来用原型加继承编写一段例子
+//"小头爸爸" 牵着  "大头儿子"  的手去吃  "饭"
+//吃完饭小头爸爸背着大头儿子回 "家"
+
+function Person(name){
+    this.name = name;
+}
+
+Person.prototype = {
+    eat : function(rice){
+        console.log(this.name + "正在吃" + rice.name);
+    },
+    handInhand : function(hand){
+        console.log(this.name + "牵着" + hand);
+    },
+    gohome : function(home){
+        console.log('回' + home.name);
+    }
+}
+
+function Rice(name){
+    this.name = name;
+}
+
+function Father(name){
+    this.name = name;
+}
+Father.prototype = new Person();
+Father.prototype.carry = function(son){
+    console.log('吃完饭' + this.name + '背着' + son);
+}
+
+function Home(name){
+    this.name = name;
+}
+
+var father = new Father('小头爸爸');
+var child = new Person('大头儿子');
+var xf = new Rice('小份饭');
+var df = new Rice('大份饭');
+var hourse = new Home('上海市中心豪华大别墅');
+father.handInhand('小头儿子的手');
+father.eat(df);
+child.eat(xf);
+father.carry(child.name);
+father.gohome(hourse);
