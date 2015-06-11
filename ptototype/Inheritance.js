6dcea49036be73a9c62846ad6fb95c46b33d9e1d
KongDongbin <kkkdb@users.noreply.github.com>
2015-06-11 23:17:47 +0800

    Update Inheritance.js

--- ptototype/Inheritance.js
+++ ptototype/Inheritance.js
@@ -12,3 +12,24 @@ Father.extends(Person,
         console.log('吃完饭' + this.name + '背着' + son);
     }
 );
+
+/*Class Person*/  
+    function Person(name){  
+        this.name = name;  
+    }  
+      
+    Person.prototype.getName = function(){  
+            return this.name;  
+    };  
+      
+    /*Class Author*/  
+    function Author(name, books){  
+        Person.call(this, name); //调用父类构造函数，第二次调用父类构造函数  
+        this.books = books;   
+    }  
+      
+    Author.prototype = new Person(); //设置原型链以获得父类的方法， 第一次调用父类构造函数  
+    Author.prototype.constructor = Author; //设置构造函数  
+    Author.prototype.getBooks = function(){  
+        return this.books;  
+    };  
