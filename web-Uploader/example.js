fde62e8bab7435bdac8938425cde270646cb638e
KongDongbin <kkkdb@users.noreply.github.com>
2015-07-02 12:05:44 +0800

    Create example.js

---
+++ web-Uploader/example.js
@@ -0,0 +1,112 @@
+var $ = jQuery,
+    $list = $('#fileList'),
+    
+    // 优化retina, 在retina下这个值是2
+            ratio = window.devicePixelRatio || 1,
+
+    // 缩略图大小
+            thumbnailWidth = 100 * ratio,
+            thumbnailHeight = 100 * ratio,
+
+    // Web Uploader实例
+            uploader;
+
+    // 初始化Web Uploader
+    uploader = WebUploader.create({
+
+        // 自动上传。
+        auto: true,
+
+        // swf文件路径
+        swf: '../js/Uploader.swf',
+
+        // 文件接收服务端。
+        server: '../../interface/songpai.php',
+
+        // 选择文件的按钮。可选。
+        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+        pick: {
+            id: '#filePicker',
+            multiple: true
+        },
+        fileVal: 'file_input[]',
+
+        fileNumLimit: 9,
+
+        // 只允许选择文件，可选。
+        accept: {
+            title: 'Images',
+            extensions: 'gif,jpg,jpeg,bmp,png',
+            mimeTypes: 'image/*'
+        }
+    });
+    
+
+    // 当有文件添加进来的时候
+    uploader.on( 'fileQueued', function( file ) {
+        var $li = $(
+                '<div id="' + file.id + '" class="file-item thumbnail">' +
+                '<img>' +
+                '<div class="info">' + file.name + '</div>' +
+                '</div>'
+                ),
+                $img = $li.find('img');
+
+        $list.append( $li );
+
+        // 创建缩略图
+        uploader.makeThumb( file, function( error, src ) {
+            if ( error ) {
+                $img.replaceWith('<span>不能预览</span>');
+                return;
+            }
+
+            $img.attr( 'src', src );
+        }, thumbnailWidth, thumbnailHeight );
+    });
+
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on( 'uploadProgress', function( file, percentage ) {
+        var $li = $( '#'+file.id ),
+                $percent = $li.find('.progress span');
+
+        // 避免重复创建
+        if ( !$percent.length ) {
+            $percent = $('<p class="progress"><span></span></p>')
+                    .appendTo( $li )
+                    .find('span');
+        }
+
+        $percent.css( 'width', percentage * 100 + '%' );
+    });
+    
+    uploader.on( 'uploadSuccess', function( file,response ) {
+        $( '#'+file.id ).addClass('upload-state-done');
+        userMainCtr.hl_pp_img = [];
+        userMainCtr.hl_pp_quick_img = [];
+        // alert(JSON.stringify(response));
+        // if(response.length){
+            for(var i =0 ;i < response[0].length; i++){
+                // var thu = response[i].replace("thu", 'big');
+                userMainCtr.hl_pp_img.push(response[1][i]);
+                userMainCtr.hl_pp_quick_img.push(response[0][i]);
+            }
+    });
+
+    // 文件上传失败，现实上传出错。
+    uploader.on( 'uploadError', function( file ) {
+        var $li = $( '#'+file.id ),
+                $error = $li.find('div.error');
+
+        // 避免重复创建
+        if ( !$error.length ) {
+            $error = $('<div class="error"></div>').appendTo( $li );
+        }
+        $error.text('上传失败');
+    });
+
+
+    // 完成上传完了，成功或者失败，先删除进度条。
+    uploader.on( 'uploadComplete', function( file ) {
+        $( '#'+file.id ).find('.progress').remove();
+    });
